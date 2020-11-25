24f38369bbcf81322815f4b09d14adfdf1fb24a2
jerry <brownjer3@gmail.com>
2020-11-25 07:51:25 -0800

    commit tree 7180e8285478b3a69c467aff1f3e9f4de4cee653

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do 
+        "My name is Jerry"
+    end
+    
+    get '/hometown' do 
+        "My hometown is Chicago"
+    end
+    
+    get '/favorite-song' do 
+        "My favorite song is Mambo #5"
+    end
 end
